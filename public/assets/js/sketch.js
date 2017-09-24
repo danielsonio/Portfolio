57d5b7f4165c6e87fadf4fcc723eb8c8006a746a
Daniel Walden <Ariel@Daniels-MacBook-Pro.local>
2017-09-23 19:37:43 -0500

    fixed doubling up

--- public/assets/js/sketch.js
+++ public/assets/js/sketch.js
@@ -1,11 +1,13 @@
 var symbol_pairs = [];
+var canvasWidth = 600;
+var canvasHeight = 400;
 
 function setup(){
-    var canvas = createCanvas(400, 400);
+    var canvas = createCanvas(canvasWidth, canvasHeight);
     canvas.parent('board');
 
-    for(var i = 0; i<2;i++) {
-        var symbol_pair = new SymbolPair(300,300);
+    for(var i = 0; i<1;i++) {
+        var symbol_pair = new SymbolPair();
         symbol_pair.generateSymbols();
         symbol_pairs.push(symbol_pair);
     }
@@ -28,11 +30,12 @@ function Symbols(x,y){
     this.range = 50;
     this.x = x;
     this.y = y;
+    this.size = 50;
     this.index0 = Math.floor((Math.random() * 2000) + 60);
     this.index1 = Math.floor((Math.random() * 2000) + 60);
     this.values = [this.index0, this.index1];
     this.symbols = [];
-    this.getSymbols = function(value){
+    this.getSymbols = function(){
         for (var i = 0;i<this.values.length;i++){
             symbol = String.fromCharCode(this.values[i]);
             this.symbols.push(symbol);
@@ -44,15 +47,15 @@ function Symbols(x,y){
 function SymbolPair(){
     this.pair_array = [];
     this.x = 0;
-    this.y = 30;
+    this.y = 50;
     this.generateSymbols = function(){
-        for (var i = 0; i<70; i++){
+        while(this.y < canvasHeight+50){
             symbol_pair = new Symbols(this.x,this.y);
             symbol_pair.getSymbols();
             this.pair_array.push(symbol_pair);
             this.x+=55;
-            console.log(symbol_pair);
-            if(this.x > 400) {
+            // console.log(symbol_pair);
+            if(this.x > canvasWidth+80) {
                 this.y += 55;
                 this.x = 0;
             }
@@ -63,7 +66,7 @@ function SymbolPair(){
     this.renderSymbols = function() {
         this.pair_array.forEach(function(symbol_pair) {
             fill(255,171,34);
-            textSize(50);
+            textSize(symbol_pair.size);
 
 
             if(mouseX>(symbol_pair.x - symbol_pair.range) && mouseX<(symbol_pair.x + symbol_pair.range) && mouseY>(symbol_pair.y - symbol_pair.range) && mouseY<(symbol_pair.y + symbol_pair.range)) {
