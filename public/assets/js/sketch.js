90e8cee861f9a6c5d9fe404e9f5a347cd1c92c62
Daniel Walden <Ariel@Daniels-MacBook-Pro.local>
2017-12-28 22:20:30 -0600

    ...

--- public/assets/js/sketch.js
+++ public/assets/js/sketch.js
@@ -1,3 +1,40 @@
+let snow = [];
+let gravity;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    gravity = createVector(0, 0.01);
+
+    for (let i = 0; i < 300; i++) {
+        let x = random(width);
+        let y = random(height);
+        snow.push(new Snowflake(x, y));
+    }
+}
+
+function draw() {
+    background(0);
+    // snow.push(new Snowflake());
+
+    let wx = map(mouseX, mouseY / 2, width, -1, 1);
+    let wind = createVector(wx, 0);
+
+    for (flake of snow) {
+        flake.applyForce(gravity);
+        flake.applyForce(wind);
+        flake.render();
+        flake.update();
+    }
+
+    // for (let i = snow.length-1; i >= 0; i--) {
+    //     if (snow[i].offScreen()) {
+    //         snow.splice(i, 1);
+    //     }
+    // }
+}
+
+
+
 function getRandomSize() {
 
     let r = pow(random(0.2, 1), 5);
